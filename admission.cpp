a4cd37ee873a24ea54e39f835c5bf2dd2de039a7
Rithik17-art <rithikag017@gmail.com>
2026-09-05 17:23:19 +0530

    Add files via upload

---
+++ admission.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int marks;
+string cource;
+int main()
+{
+    cout << "enter the cource name: ";
+    cin >> cource;
+
+    cout << "enter the marks: ";
+    cin >> marks;
+    if (cource == "engineering")
+    {
+        if (marks >=90)
+        {
+            cout << "you are admitted to engineering cource";
+        }    
+        else cout << "marks not sufficient for admission";
+    }    
+    else if (cource == "b.com")
+    {
+        if (marks >=70)
+        {
+            cout << "you are admitted to b.com cource";
+        }  
+        else
+        {
+            cout << "marks not sufficient for admission";
+        }
+    }    
+    return 0;
+
+}
